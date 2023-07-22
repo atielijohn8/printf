eb0323b23b91bd44093d6d72209d42c6a50a0f8b
atielijohn8 <atielijoh4@gmail.com>
2023-07-22 15:57:25 -0700

    PRINT.C

---
+++ PRINT.C
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdint.h>
+/**
+ * _printf - function printing output according to format
+ * @format: the format
+ * Return: value
+ */
+
+int _printf(const char *format, ...)
+{
+	int prints_chars;
+
+	conver_t funct_list[] =	{
+		{"s", p_string},
+		{"c", p_char},
+		{"%", p_percent},
+		{"d", p_integer},
+		{"i", p_integer},
+		{NULL, NULL}
+	};
+
+	va_list args;
+
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+
+	prints_chars = parser(format, funct_list, args);//calling paser function
+	va_end(args);
+
+	return (prints_chars);
+}
